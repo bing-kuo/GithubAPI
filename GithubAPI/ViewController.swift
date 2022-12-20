535034c90083e6724a13508107b4f6c54eec62de
bing <bindaguo@gmail.com>
2022-12-20 22:26:28 +0800

    Initial Commit

---
+++ GithubAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubAPI
+//
+//  Created by Bing Guo on 2022/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
